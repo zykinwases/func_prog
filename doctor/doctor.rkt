db8fce9234ba3acefb6d9de920b4229eaa4c6e2b
Zykin Yaroslav <zykin@ispras.ru>
2020-11-22 12:54:23 +0300

    IO remake done

--- doctor/doctor.rkt
+++ doctor/doctor.rkt
@@ -1,16 +1,17 @@
 ; заготовка "Доктора". Август 2019
 #lang scheme/base
 ; В учебных целях используется базовая версия Scheme
-
+(require scheme/string)
+(require racket/format)
 ; основная функция, запускающая "Доктора"
 ; параметр stop-word -- стоп-слово, при вводе которого в качестве имени пациента доктор завершает приём
 ; параметр max-patients -- максимальное количество пациентов, которое принимает доктор
 (define (visit-doctor stop-word max-patients)
-  (if (= max-patients 0) (print '(its time to go home))
+  (if (= max-patients 0) (printf "its time to go home")
       (let ((name(ask-patient-name)))
-         (if (eq? name stop-word) (print '(its time to go home))
+         (if (equal? name (~a stop-word)) (printf "its time to go home")
              (begin (printf "Hello, ~a!\n" name)
-                    (print '(what seems to be the trouble?))
+                    (printf "what seems to be the trouble?\n")
                     (doctor-driver-loop name '())
                     (visit-doctor stop-word (- max-patients 1)))
              )
@@ -21,10 +22,10 @@
 ; функция, спрашивающая имя следующего пациента
 (define (ask-patient-name)
  (begin
-  (println '(next!))
-  (println '(who are you?))
+  (printf "next!\n")
+  (printf "who are you?\n")
   (print '**)
-  (car (read))
+  (car (filter non-empty-string? (string-split (read-line) #px"\\s*\\b\\s*")))
  ) 
 )
 
@@ -35,17 +36,36 @@
 (define (doctor-driver-loop name history)
     (newline)
     (print '**) ; доктор ждёт ввода реплики пациента, приглашением к которому является **
-    (let ((user-response (read)))
+    (let ((user-response (read-response (read-line))))
       (cond 
-	    ((equal? user-response '(goodbye)) ; реплика '(goodbye) служит для выхода из цикла
+	    ((equal? (car user-response) '("goodbye")) ; если первое предложение - '(goodbye), то прощаемся
              (printf "Goodbye, ~a!\n" name)
-             (printf "(see you next week)\n"))
-            (else (print (reply user-response history strategies)) ; иначе Доктор генерирует ответ, печатает его и продолжает цикл
-                  (doctor-driver-loop name (cons user-response history))
+             (printf "see you next week\n"))
+            (else (printf (make-answer (reply user-response history strategies))) ; иначе Доктор генерирует ответ, печатает его и продолжает цикл
+                  (doctor-driver-loop name (append user-response history))
              )
        )
       )
 )
+
+(define (help)
+  (print (make-answer (car (read-response (read-line))))))
+
+(define (read-response str)
+  (map (lambda (sent) (filter non-empty-string? (string-split sent #px"\\s*\\b\\s*")))
+       (string-split str #px"\\.|\\?|!")
+   )
+  )
+
+(define (make-answer str)
+  (let form ((cur str) (res '()))
+    (if (null? cur) (string-join (reverse res))
+        (if (regexp-match-exact? #px"\\W+" (car cur)) (form (cdr cur) (cons (string-append (car res) (car cur)) (cdr res)))
+            (form (cdr cur) (cons (car cur) res))
+         )
+        )
+    )
+  )
 
 ; генерация ответной реплики по user-response -- реплике от пользователя
 ; history -- история реплик, strategies -- используемые стратегии
@@ -77,17 +97,17 @@
         ;(many-replace                   ; начальный вариант
         ;(many-replace-iter              ; итеративный вариант
         (many-replace-map               ; вариант с функцией высшего порядка
-                        '((am are)
-                        (are am)
-                        (i you)
-                        (me you)
-                        (mine yours)
-                        (my your)
-						(myself yourself)
-                        (you i)
-                        (your my)
-                        (yours mine)
-						(yourself myself))
+                        '(("am" "are")
+                        ("are" "am")
+                        ("i" "you")
+                        ("me" "you")
+                        ("mine" "yours")
+                        ("my" "your")
+						("myself" "yourself")
+                        ("you" "i")
+                        ("your" "my")
+                        ("yours" "mine")
+						("yourself" "myself"))
                       phrase)
  )
 
@@ -144,15 +164,15 @@
   )
 
 (define (qualifier-answer user-response)
-        (append (pick-random '((you seem to think that)
-                               (you feel that)
-                               (why do you believe that)
-                               (why do you say that)
-                               (what made you regret that)
-                               (its interesting that)
-                               (why do you think that))
+        (append (pick-random '(("you seem to think that")
+                               ("you feel that")
+                               ("why do you believe that")
+                               ("why do you say that")
+                               ("what made you regret that")
+                               ("its interesting that")
+                               ("why do you think that"))
                 )
-                (change-person user-response)
+                (change-person (car user-response))
         )
  )
 
@@ -164,13 +184,13 @@
   )
 
 (define (hedge)
-       (pick-random '((please go on)
-                       (many people have the same sorts of feelings)
-                       (many of my patients have told me the same thing)
-                       (please continue)
-                       (every human went through the same situation)
-                       (please do not stop)
-                       (what was the reason of saying this))
+       (pick-random '(("please go on")
+                       ("many people have the same sorts of feelings")
+                       ("many of my patients have told me the same thing")
+                       ("please continue")
+                       ("every human went through the same situation")
+                       ("please do not stop")
+                       ("what was the reason of saying this"))
          )
 )
 
@@ -182,9 +202,9 @@
   )
 
 (define (history-answer history)
-  (append '(earlier you said that)         
+  (cons "earlier you said that"         
           (change-person (pick-random history))
-          )     
+          )
   )
 
 ; 4й способ генерации ответной реплики -- ключевые слова
@@ -197,43 +217,43 @@
 ; ключевые слова в виде ((<ключевые слова> <шаблоны>) ... )
 (define patterns '( 
                    ( ; начало данных 1й группы
-                    (depressed suicide exams university depression) ; список ключевых слов 1й группы
+                    ("depressed" "suicide" "exams" "university" "depression") ; список ключевых слов 1й группы
                     ( ; список шаблонов для составления ответных реплик 1й группы 
-                     (when you feel depressed, go out for ice cream)
-                     (depression is a disease that can be treated)
-                     (you should say more about this)
+                     ("when you feel depressed, go out for ice cream")
+                     ("depression is a disease that can be treated")
+                     ("you should say more about this")
                      )
                     ) ; завершение данных 1й группы
                    ( ; начало данных 2й группы ...
-                    (mother father parents brother sister uncle ant grandma grandpa)
+                    ("mother" "father" "parents" "brother" "sister" "uncle" "ant" "grandma" "grandpa")
                     (
-                     (tell me more about your * , i want to know all about your *)
-                     (why do you feel that way about your * ?)
-                     (family is important for everyone)
-                     (is everything ok with your *)
+                     ("tell me more about your" * ", i want to know all about your" *)
+                     ("why do you feel that way about your" * "?")
+                     ("family is important for everyone")
+                     ("is everything ok with your" *)
                      )
                     )
                    (
-                    (university scheme lections lessons)
+                    ("university" "scheme" "lections" "lessons")
                     (
-                     (your education is important)
-                     (how many time do you spend to learning ?)
-                     (you should put more effort to *)
+                     ("your education is important")
+                     ("how many time do you spend to learning ?")
+                     ("you should put more effort to" *)
                      )
                     )
                    (
-                    (love affection passion tenderness softness)
+                    ("love" "affection" "passion" "tenderness" "softness")
                     (
-                     (Feeling * is great)
-                     (Sometimes such feelings can be painfull)
-                     (Make * not war)
+                     ("Feeling" * "is great")
+                     ("Sometimes such feelings can be painfull")
+                     ("Make" * "not war")
                      )
                     )
                    (
-                    (good bad nice normal ok)
+                    ("good" "bad" "nice" "normal" "ok")
                     (
-                     (you say general words , be more detailed please)
-                     (why do you say , that it is *)
+                     ("you say general words , be more detailed please")
+                     ("why do you say , that it is" *)
                      )
                     )
                    )
@@ -288,3 +308,10 @@
 (define (strategy-predicate strategy) (list-ref strategy 0))
 (define (strategy-weight strategy) (list-ref strategy 1))
 (define (strategy-body strategy) (list-ref strategy 2))
+
+
+(define (kuku)
+  (let ((lol (read-line)))
+    (print (filter non-empty-string? (string-split (read-line) #px"\\s*\\b\\s*"))) ;#px"\\s*(\\.|!|\\?)\\s*"))
+    )
+  )
